555beb057ae3ed05bbde324c009d7064af23da64
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-10-05 17:15:04 +0200

    en cours

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Button } from "./components/Button"
+import { Image } from "./components/Image"
 
 function App() {
 
 
   return (
     <>
-  
+  <div>
+      <Button text="Bubu" />
+      <Button text="bababa" />
+      <Image scr='https://media.inmobalia.com/imgV1/B98Le8~d7M9k3DegigWkzHXQlgzMFGqGJJp6ZRUcpX033lqadFBp2i4GGW4X2J1jIJ9Pwc6GsJX5cPSaC8Y5L~JfyHds9HljFKbIihOkX3Hq5C9d69~GwVQAUJ3F~nk7fQh0UAqAYi7r3tGF4MzRyAahwYUUdBGcpIv62graeAG8w~8wUqYBXzLX2fvWEnUTQLuqRdONy5WACPmyO8uqKAHjHemV6z~5jgt8AQg7cew~6RB3PVf3qGWecjwBIeE1gPVvPLV3hrMoCZbpm9URzDnYisb5CD3BHYx3qe9hkFGi2MpOpzK2DLeQMSnujni41g--.jpg'/>
+    </div>
     </>
   )
 }
