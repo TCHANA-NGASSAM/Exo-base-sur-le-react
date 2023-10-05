7e19e1ef866738f92bba1c4e45c40edb0624a597
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-10-05 15:37:54 +0200

    en cours

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { PrimaryButton } from "./components/PrimaryButton"
-import { TextField } from "./components/TextField"
 
 function App() {
 
 
   return (
     <>
-   <PrimaryButton/>
-   <TextField/>
-   <TextField/>
+  
     </>
   )
 }
